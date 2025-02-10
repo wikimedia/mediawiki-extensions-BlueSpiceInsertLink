3266c703654ba8f29c894cd929ad8bce3d09c0a4
hslater <hamish.slater@hallowelt.com>
2025-02-10 21:10:15 +0100

    Update BeforePageDisplayHook namespace
    ERM38444
    
    [REL1_43, master]

--- src/Hook/AddConfigs.php
+++ src/Hook/AddConfigs.php
@@ -3,7 +3,7 @@
 namespace BlueSpice\InsertLink\Hook;
 
 use MediaWiki\Config\ConfigFactory;
-use MediaWiki\Hook\BeforePageDisplayHook;
+use MediaWiki\Output\Hook\BeforePageDisplayHook;
 
 class AddConfigs implements BeforePageDisplayHook {
 
